b37048875a423a27501e28b36eca5ea76e03db85
David Backeus <duztdruid@gmail.com>
2015-07-28 13:54:27 +0200

    Request with #post?

--- src/startram.cr
+++ src/startram.cr
@@ -5,7 +5,22 @@ require "./startram/*"
 module Startram
   class App < HTTP::Handler
     def call(request)
-      HTTP::Response.ok("text/plain", "Initiating hyperdrive...")
+      request = Request.new(request)
+
+      if request.post?
+        HTTP::Response.new(201, "Hyperdrive initiated...", HTTP::Headers{"Content-type": "text/plain"})
+      else
+        HTTP::Response.ok("text/plain", "Hyperdriving!")
+      end
+    end
+  end
+
+  class Request
+    def initialize(@request)
+    end
+
+    def post?
+      @request.method == "POST"
     end
   end
 end
