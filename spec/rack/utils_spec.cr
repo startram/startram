8d569602f85d7dba44ea9e1c4a1d0dd78f82866e
David Backeus <duztdruid@gmail.com>
2015-08-01 14:17:05 +0200

    Implement Rack::Utils.parse_nested_query and friends

---
+++ spec/rack/utils_spec.cr
@@ -0,0 +1,108 @@
+require "../spec_helper"
+require "../../src/rack/utils"
+
+module Rack
+  module Utils
+    describe ".parse_query" do
+      it "parses query strings correctly" do
+        parse_query("foo=bar").should eq({ "foo" => "bar" })
+        parse_query("foo=\"bar\"").should eq({ "foo" => "\"bar\"" })
+        parse_query("foo=bar&foo=quux").should eq({ "foo" => ["bar", "quux"] })
+        parse_query("foo=1&bar=2").should eq({ "foo" => "1", "bar" => "2" })
+        parse_query("my+weird+field=q1%212%22%27w%245%267%2Fz8%29%3F").should eq({
+          "my weird field" => "q1!2\"'w$5&7/z8)?"
+        })
+        parse_query("foo%3Dbaz=bar").should eq({ "foo=baz" => "bar" })
+        parse_query("=").should eq({ "" => "" })
+        parse_query("=value").should eq({ "" => "value" })
+        parse_query("key=").should eq({ "key" => "" })
+        parse_query("&key&").should eq({ "key" => "" }) # nil value in ruby
+        parse_query(";key;", ";,").should eq({ "key" => "" }) # nil value in ruby
+        parse_query(",key,", ";,").should eq({ "key" => "" }) # nil value in ruby
+        parse_query(";foo=bar,;", ";,").should eq({ "foo" => "bar" })
+        parse_query(",foo=bar;,", ";,").should eq({ "foo" => "bar" })
+      end
+    end
+
+    describe ".parse_nested_query" do
+      it "parses shallow strings" do
+        parse_nested_query("foo").should eq({ "foo" => "" }) # nil in ruby
+        parse_nested_query("foo=").should eq({ "foo" => "" })
+        parse_nested_query("foo=bar").should eq({ "foo" => "bar" })
+        parse_nested_query("foo=\"bar\"").should eq({ "foo" => "\"bar\"" })
+
+        parse_nested_query("foo=bar&foo=quux").should eq({ "foo" => "quux" })
+        parse_nested_query("foo&foo=").should eq({ "foo" => "" })
+        parse_nested_query("foo=1&bar=2").should eq({ "foo" => "1", "bar" => "2" })
+        parse_nested_query("&foo=1&&bar=2").should eq({ "foo" => "1", "bar" => "2" })
+        parse_nested_query("foo&bar=").should eq({ "foo" => "", "bar" => "" }) # foo => nil in ruby
+        parse_nested_query("foo=bar&baz=").should eq({ "foo" => "bar", "baz" => "" })
+        parse_nested_query("my+weird+field=q1%212%22%27w%245%267%2Fz8%29%3F").should eq({
+          "my weird field" => "q1!2\"'w$5&7/z8)?"
+        })
+
+        parse_nested_query("a=b&pid%3D1234=1023").should eq({ "pid=1234" => "1023", "a" => "b" })
+      end
+
+      it "parses shallow arrays" do
+        parse_nested_query("foo[]").should eq({ "foo" => [""] }) # [nil] in ruby
+        parse_nested_query("foo[]=").should eq({ "foo" => [""] })
+        parse_nested_query("foo[]=bar").should eq({ "foo" => ["bar"] })
+        parse_nested_query("foo[]=bar&foo").should eq({ "foo" => "" }) # nil in ruby
+        parse_nested_query("foo[]=bar&foo[").should eq({ "foo" => ["bar"], "foo[" => "" }) # foo[ => nil in ruby
+        parse_nested_query("foo[]=bar&foo[=baz").should eq({ "foo" => ["bar"], "foo[" => "baz" })
+        parse_nested_query("foo[]=bar&foo[]").should eq({ "foo" => ["bar", ""] }) # "bar", nil in ruby
+        parse_nested_query("foo[]=bar&foo[]=").should eq({ "foo" => ["bar", ""] })
+
+        parse_nested_query("foo[]=1&foo[]=2").should eq({ "foo" => ["1", "2"] })
+        parse_nested_query("foo=bar&baz[]=1&baz[]=2&baz[]=3").should eq({
+          "foo" => "bar", "baz" => ["1", "2", "3"]
+        })
+        parse_nested_query("foo[]=bar&baz[]=1&baz[]=2&baz[]=3").should eq({
+          "foo" => ["bar"], "baz" => ["1", "2", "3"]
+        })
+      end
+
+      it "parses nested queries" do
+        parse_nested_query("x[y]=1").should eq({ "x" => {"y" => "1"} })
+        parse_nested_query("x[y][z]=1").should eq({ "x" => {"y" => {"z" => "1"}} })
+        parse_nested_query("x[y][z][]=1").should eq({ "x" => {"y" => {"z" => ["1"]}} })
+        parse_nested_query("x[y][z]=1&x[y][z]=2").should eq({ "x" => {"y" => {"z" => "2"}} })
+        parse_nested_query("x[y][z][]=1&x[y][z][]=2").should eq({ "x" => {"y" => {"z" => ["1", "2"]}} })
+
+        parse_nested_query("x[y][][z]=1").should eq({ "x" => {"y" => [{"z" => "1"}]} })
+        parse_nested_query("x[y][][z][]=1").should eq({ "x" => {"y" => [{"z" => ["1"]}]} })
+        parse_nested_query("x[y][][z]=1&x[y][][w]=2").should eq({ "x" => {"y" => [{"z" => "1", "w" => "2"}]} })
+
+        parse_nested_query("x[y][][v][w]=1").should eq({ "x" => {"y" => [{"v" => {"w" => "1"}}]} })
+        parse_nested_query("x[y][][z]=1&x[y][][v][w]=2").should eq({ "x" => {"y" => [{"z" => "1", "v" => {"w" => "2"}}]} })
+
+        parse_nested_query("x[y][][z]=1&x[y][][z]=2").should eq({ "x" => {"y" => [{"z" => "1"}, {"z" => "2"}]} })
+        parse_nested_query("x[y][][z]=1&x[y][][w]=a&x[y][][z]=2&x[y][][w]=3").should eq({ "x" => {"y" => [{"z" => "1", "w" => "a"}, {"z" => "2", "w" => "3"}]} })
+      end
+
+      it "raises errors on format inconsistencies" do
+        expect_raises ParameterTypeError, "expected Hash (got String) for param `y'" do
+          parse_nested_query("x[y]=1&x[y]z=2")
+        end
+
+        expect_raises ParameterTypeError, /expected Array \(got .*\) for param `x'/ do
+          parse_nested_query("x[y]=1&x[]=1")
+        end
+
+        expect_raises ParameterTypeError, "expected Array (got String) for param `y'" do
+          parse_nested_query("x[y]=1&x[y][][w]=2")
+        end
+      end
+    end
+
+    describe ".unescape" do
+      it "escapes strings" do
+        unescape("fo%3Co%3Ebar").should eq "fo<o>bar"
+        unescape("a+space").should eq "a space"
+        unescape("a%20space").should eq "a space"
+        unescape("q1%212%22%27w%245%267%2Fz8%29%3F%5C").should eq "q1!2\"'w$5&7/z8)?\\"
+      end
+    end
+  end
+end
