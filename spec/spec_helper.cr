f0e00508cd733965f2c5ab17b5e9709a7bcee936
David Backeus <duztdruid@gmail.com>
2015-08-07 01:13:40 +0200

    Log level warn in test env

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,6 +1,8 @@
 require "spec"
 require "../src/startram"
 
+Startram.log.level = Logger::WARN
+
 def build_request(method, path, body = "Test", headers = HTTP::Headers.new)
   base_request = HTTP::Request.new(method, path, headers, body)
 
